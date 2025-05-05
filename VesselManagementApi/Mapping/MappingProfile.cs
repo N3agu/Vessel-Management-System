1b80b52ebeec0a5cffa03d6ca27114b1a6634815
N3agu <andreei1465@yahoo.com>
2025-05-05 12:27:11 +0000

    Create AutoMapper profile

---
+++ VesselManagementApi/Mapping/MappingProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using VesselManagementApi.DTOs;
+using VesselManagementApi.Models;
+
+namespace VesselManagementApi.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Owner Mappings
+            CreateMap<Owner, OwnerDto>();
+            CreateMap<CreateOwnerDto, Owner>();
+
+            // Ship Mappings
+            CreateMap<Ship, ShipDto>();
+
+            // Map Ship to ShipDetailsDto, including Owner information
+            CreateMap<Ship, ShipDetailsDto>()
+                .ForMember(dest => dest.Owners,
+                opt => opt.MapFrom(src => src.ShipOwners.Select(so => so.Owner)));
+
+            CreateMap<CreateShipDto, Ship>(); // Map CreateShipDto to Ship (OwnerIds handled separately in service)
+            CreateMap<UpdateShipDto, Ship>(); // Map UpdateShipDto to Ship
+        }
+    }
+}
